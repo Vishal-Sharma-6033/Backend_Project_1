01a726954ea1f2dcab41058b35565736128b69a1
Vishal Sharma <vs2520329@gmail.com>
2026-01-25 16:34:45 +0530

    Database Connection successfully

---
+++ src/index.js
@@ -0,0 +1,41 @@
+import dotenv from "dotenv";
+import connectToDB from "./db/index.js";
+
+dotenv.config({
+    path: './env'
+});
+
+connectToDB();
+
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express";
+const app=express();
+
+(async()=>{
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+        console.log("Connected to MongoDB successfully");
+        app.on("error",(error)=>{
+            console.error("Error in Express app:",error);
+            throw error;
+        })
+
+        app.listen(process.env.PORT,()=>{
+            console.log(`Server is running on port ${process.env.PORT}`);
+        })
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+        throw error;
+    }
+})()
+*/
