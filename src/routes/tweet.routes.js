4664cb5b924a40d8a7cc6bdf4747e1dc366086f0
Vishal Sharma <vs2520329@gmail.com>
2026-02-10 02:24:19 +0530

    Update tweet.routes.js

--- src/routes/tweet.routes.js
+++ src/routes/tweet.routes.js
@@ -3,10 +3,11 @@ import { verifyJWT } from "../middlewares/auth.middlewares.js";
 import { createTweet, deleteTweet, getUserTweet, updateTweet } from "../controllers/tweet.controllers.js";
 
 const router=Router()
+router.use(verifyJWT)
 
-router.route("/").post(verifyJWT,createTweet)
-router.route("/:id").delete(verifyJWT,deleteTweet)
-router.route("/:id").patch(verifyJWT,updateTweet)
-router.route("/:id").get(verifyJWT,getUserTweet)
+router.route("/").post(createTweet)
+router.route("/:id").delete(deleteTweet)
+router.route("/:id").patch(updateTweet)
+router.route("/:id").get(getUserTweet)
 
 export default router;
